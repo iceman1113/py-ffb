4ece5310eb35b6318d7600bf3f15f67420025c78
iceman1113 <iceman111322@gmail.com>
2021-07-26 23:57:26 -0400

    initial stub of model/player

---
+++ ffb/model/player.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+class Player:
+    pos = None
+    name = None
+    tm = None
+    bye = None
+    rank = None
+    points = None
+    risk = None
+    adp = None
+    tier = None
+    notes = None
+
+    def __init__(self):
+        pass
